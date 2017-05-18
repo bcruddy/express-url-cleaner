b03c096bc02bcc824b5e83b351153aa77605dacd
Brian Ruddy <briancruddy@gmail.com>
2017-05-18 09:08:14 -0400

    run same tests with different module in a for loop

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,96 +3,51 @@ const App = require('./app'),
     urlCleaner = require('../lib'),
     urlCleanerES5 = require('../lib/es5');
 
-describe('the express-url-cleaner module', function () {
-    let app;
+[urlCleaner, urlCleanerES5].forEach(cleaner => {
+    describe('the express-url-cleaner module', function () {
+        let app;
 
-    before(() => {
-        app = App(urlCleaner)
-    });
-
-    describe('baseline', function () {
-        it('GET /path', done => {
-            request(app)
-                .get('/path')
-                .expect(200, done);
-        });
-    });
-
-    describe('sanitizes html in the pathname', function () {
-        it('GET /path/<script>test</script>', done => {
-            request(app)
-                .get('/path<script>test</script>')
-                .expect('location', '/path')
-                .expect(301, done);
-        });
-    });
-
-    describe('sanitizes html in the search', function () {
-        it('GET /path?key=value - 200', done => {
-            request(app)
-                .get('/')
-                .expect(200, done);
+        before(() => {
+            app = App(cleaner);
         });
 
-        it('GET /path?key=<script>alert("hello!")</script> - 301', done => {
-            request(app)
-                .get('/path?key=<script>alert("hello!")</script>')
-                .expect('location', '/path?key=')
-                .expect(301, done);
+        describe('baseline', function () {
+            it('GET /path', done => {
+                request(app)
+                    .get('/path')
+                    .expect(200, done);
+            });
         });
 
-        it('GET /path?<script>alert("hello!")</script> - 301', done => {
-            request(app)
-                .get('/path?<script>alert("hello!")</script>')
-                .expect('location', '/path?')
-                .expect(301, done);
-        });
-    });
-});
-
-describe('the express-url-cleaner ES5 module', function () {
-    let app;
-
-    before(() => {
-        app = App(urlCleanerES5)
-    });
-
-    describe('baseline', function () {
-        it('GET /path', done => {
-            request(app)
-                .get('/path')
-                .expect(200, done);
-        });
-    });
-
-    describe('sanitizes html in the pathname', function () {
-        it('GET /path/<script>test</script>', done => {
-            request(app)
-                .get('/path<script>test</script>')
-                .expect('location', '/path')
-                .expect(301, done);
-        });
-    });
-
-    describe('sanitizes html in the search', function () {
-        it('GET /path?key=value - 200', done => {
-            request(app)
-                .get('/')
-                .expect(200, done);
+        describe('sanitizes html in the pathname', function () {
+            it('GET /path/<script>test</script>', done => {
+                request(app)
+                    .get('/path<script>test</script>')
+                    .expect('location', '/path')
+                    .expect(301, done);
+            });
         });
 
-        it('GET /path?key=<script>alert("hello!")</script> - 301', done => {
-            request(app)
-                .get('/path?key=<script>alert("hello!")</script>')
-                .expect('location', '/path?key=')
-                .expect(301, done);
-        });
+        describe('sanitizes html in the search', function () {
+            it('GET /path?key=value - 200', done => {
+                request(app)
+                    .get('/')
+                    .expect(200, done);
+            });
 
-        it('GET /path?<script>alert("hello!")</script> - 301', done => {
-            request(app)
-                .get('/path?<script>alert("hello!")</script>')
-                .expect('location', '/path?')
-                .expect(301, done);
+            it('GET /path?key=<script>alert("hello!")</script> - 301', done => {
+                request(app)
+                    .get('/path?key=<script>alert("hello!")</script>')
+                    .expect('location', '/path?key=')
+                    .expect(301, done);
+            });
+
+            it('GET /path?<script>alert("hello!")</script> - 301', done => {
+                request(app)
+                    .get('/path?<script>alert("hello!")</script>')
+                    .expect('location', '/path?')
+                    .expect(301, done);
+            });
         });
     });
 });
