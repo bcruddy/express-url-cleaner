fa7b3d89ffe663d75c8836fb11841143dc73f926
Brian Ruddy <briancruddy@gmail.com>
2017-05-26 08:19:07 -0400

    arrow functions for describe blocks, add more test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,31 +4,43 @@ const App = require('./app'),
     urlCleanerES5 = require('../lib/es5');
 
 [urlCleaner, urlCleanerES5].forEach(cleaner => {
-    describe('the express-url-cleaner module', function () {
+    describe('the express-url-cleaner module', () => {
         let app;
 
         beforeAll(() => {
             app = App(cleaner);
         });
 
-        describe('baseline', function () {
-            it('GET /path', done => {
+        describe('sanitizes html in the pathname', () => {
+            it('GET /path - 200', done => {
                 request(app)
                     .get('/path')
                     .expect(200, done);
             });
-        });
 
-        describe('sanitizes html in the pathname', function () {
-            it('GET /path/<script>test</script>', done => {
+            it('GET /path/<script>test</script> - 301', done => {
                 request(app)
                     .get('/path<script>test</script>')
                     .expect('location', '/path')
                     .expect(301, done);
             });
+
+            it('GET /path/<script>test</script> - 301', done => {
+                request(app)
+                    .get('/path/<script>test</script>')
+                    .expect('location', '/path')
+                    .expect(301, done);
+            });
+
+            it('GET /path/<script>test</script>/extra - 301', done => {
+                request(app)
+                    .get('/path/<script>test</script>/extra')
+                    .expect('location', '/path')
+                    .expect(301, done);
+            });
         });
 
-        describe('sanitizes html in the search', function () {
+        describe('sanitizes html in the search', () => {
             it('GET /path?key=value - 200', done => {
                 request(app)
                     .get('/')
@@ -39,6 +51,13 @@ const App = require('./app'),
                 request(app)
                     .get('/path?key=<script>alert("hello!")</script>')
                     .expect('location', '/path?key=')
+                    .expect(301, done);
+            });
+
+            it('GET /path?key=<script>alert("hello!")</script>&key2=value - 301', done => {
+                request(app)
+                    .get('/path?key=<script>alert("hello!")</script>&key2=value')
+                    .expect('location', '/path?key=&key2=value')
                     .expect(301, done);
             });
 
